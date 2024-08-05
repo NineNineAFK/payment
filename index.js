61aaf073bbaf7cf320f03b69414f29f433556487
Aadity Dawkar <dawkarad2002@gmail.com>
2024-08-06 01:29:35 +0530

    initial commit

--- index.js
+++ index.js
@@ -9,3 +9,12 @@ dotenv.config();
 const app = express();
 
 app.use(bodyParser.json());
+app.use(cors());
+app.use(express.static('static'));
+app.set('view engine', 'ejs');
+
+
+mongoose.connect(process.env.MONGODB_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  });
